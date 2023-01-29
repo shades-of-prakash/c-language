438feed8a0d6ae74fd6101e09d31d347296c0ab5
shadesofprakash <prakashvarma00045@gmail.com>
2023-01-29 11:59:03 +0530

    matirces

---
+++ menudrivens/measuresofcentraltendency.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+       int data[10];
+       int i,j,n;
+       printf("enter number of obeservations\n");
+       scanf("%d",&n);
+       for(i=0;i<n;i++){
+               scanf("%d",&data[i]);
+       }
+       while(1){
+
+        
+       }
+
+
+}
