e1f0fda951c16677c5b0b3d476d5073f1500dd75
shadesofprakash <prakashvarma00045@gmail.com>
2023-01-31 19:45:38 +0530

    first

---
+++ menudrivens/menudrivenforseries.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+        int x,n;
+        printf("enter x and and n value\n");
+        scanf("%d%d",&x,&n);
+        while(1){
+            printf("enter your choice\n 1.cosx\n 2.sinx\n 3.e");
+        }
+
+}
